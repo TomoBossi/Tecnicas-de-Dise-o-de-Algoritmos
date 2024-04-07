511fd22cbf4300a3669c6fbc18d71545cfe923e6
TomoBossi <tomasbossi97@gmail.com>
2024-04-06 23:15:54 -0300

    mejorar entregas

--- entregable02-UVA-11790_dynamic_programming_topdown.cpp
+++ entregable02-UVA-11790_dynamic_programming_topdown.cpp
@@ -20,62 +20,62 @@ void insert_in_order(int N, int M[][2], int data[], int i) {
     }
 }
 
-void longest_subsequence(int N, int M[][2], int heights[], int widths[], int building_idx, bool reverse) {
+void longest_subsequence(int N, int M[][2], int hs[], int ws[], int building_idx, bool reverse) {
     if (building_idx < 0) {
         return;
     }
-    int h = heights[building_idx];
-    int w = widths[building_idx];
+    int h = hs[building_idx];
+    int w = ws[building_idx];
     for (int k = building_idx+1; k < N; k++) {
         int total_width = M[k][0];
         int building = M[k][1];
-        bool subsequence_continues = (!reverse && h < heights[building] || reverse && h > heights[building]);
+        bool subsequence_continues = (!reverse && h < hs[building] || reverse && h > hs[building]);
         if (subsequence_continues) {
             int data[2] = {w + total_width, building_idx};
             insert_in_order(N, M, data, building_idx);
-            longest_subsequence(N, M, heights, widths, building_idx - 1, reverse);
+            longest_subsequence(N, M, hs, ws, building_idx - 1, reverse);
             return;
         }
     }
     int data[2] = {w, building_idx};
     insert_in_order(N, M, data, building_idx);
-    longest_subsequence(N, M, heights, widths, building_idx - 1, reverse);
+    longest_subsequence(N, M, hs, ws, building_idx - 1, reverse);
 }
 
 int main() { 
-    int cases;
-    cin >> cases;
-    for (int case_idx = 0; case_idx < cases; case_idx++) {
+    int c;
+    cin >> c;
+    for (int ci = 0; ci < c; ci++) {
         int N;
         cin >> N;
-        int heights[N];
-        int widths[N];
+        int hs[N];
+        int ws[N];
         int number;
         int i = 0;
         while (i < N) {
             cin >> number; 
-            heights[i++] = number;
+            hs[i++] = number;
         }
         i = 0;
         while (i < N) {
             cin >> number; 
-            widths[i++] = number;
+            ws[i++] = number;
         }
 
         int M[N][2];
-        M[N-1][0] = widths[N-1];
+        M[N-1][0] = ws[N-1];
         M[N-1][1] = N-1;
-        longest_subsequence(N, M, heights, widths, N-2, false);
+        longest_subsequence(N, M, hs, ws, N-2, false);
         int inc = M[0][0];
-        M[N-1][0] = widths[N-1];
+        M[N-1][0] = ws[N-1];
         M[N-1][1] = N-1;
-        longest_subsequence(N, M, heights, widths, N-2, true);
+        longest_subsequence(N, M, hs, ws, N-2, true);
         int dec = M[0][0];
 
         if (inc >= dec) {
-            cout << "Case " << case_idx+1 << ". Increasing (" << inc << "). Decreasing (" << dec << ").\n";
+            cout << "Case " << ci+1 << ". Increasing (" << inc << "). Decreasing (" << dec << ").\n";
         } else {
-            cout << "Case " << case_idx+1 << ". Decreasing (" << dec << "). Increasing (" << inc << ").\n";
+            cout << "Case " << ci+1 << ". Decreasing (" << dec << "). Increasing (" << inc << ").\n";
         }
     }
     return 0;
--- entregable03-UVA-1231_dynamic_programming_bottomup.cpp
+++ entregable03-UVA-1231_dynamic_programming_bottomup.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
 int main(){
-    ios_base::sync_with_stdio(false);                                           // Optimize reading from stdin
-    int c;                                                                      // Number of test cases
-    cin >> c;
-    while (c--){                                                                // While there are test cases remaining...
-        int res = 0;                                                            // Initilize test case result
-        int T, H, F, q, ai, prev;
-        cin >> T >> H >> F;                                                     // Number of trees, height of each tree, falling distance
-        vector<vector<int>> A(T, vector<int>(H, 0));                            // Number of acorns at each height of each tree
-        vector<vector<int>> M(T, vector<int>(H, 0));                            // Memorization matrix, holds the max possible acorns starting from any given tree and height
-        vector<int> hMax(H, 0);                                                 // Memorization vector, holds the max possible acorns at each height across all trees
-        for (int ti = 0; ti < T; ti++){                                         // Initialize A
+    ios_base::sync_with_stdio(false);                                           // ...
+    cin.tie(NULL);                                                              // Optimize reading from stdin (970 ms -> 160 ms in vjudge)
+    int C, T, H, F, q, a;
+    cin >> C;                                                                   // Number of test cases
+    while (C--){                                                                // While there are test cases remaining...
+        cin >> T >> H >> F;                                                     // Number of trees, height of each tree, falling distance                         
+        int M[T+1][H] = {};                                                     // M is the memorization matrix, which holds the max acorns attainable starting from any given tree and height. The last subarray holds the max possible acorns from each height across all trees (unnecessary, but it can save lots of time). The solution, after computing M, is therefore given by M[T][H-1]
+        for (int t = 0; t<T; t++){                                              // Initialize M with the number of acorns at each height of each tree
             cin >> q;
             while (q--){
-                cin >> ai;
-                A[ti][ai-1]++;
+                cin >> a;
+                M[t][a-1]++;
             }
         }
-        for (int hi = 0; hi < H; hi++){
-            for (int ti = 0; ti < T; ti++){
-                M[ti][hi] = A[ti][hi];                                          // M[ti][hi], the max acorns attainable from tree ti and height hi, is the acorns at A[ti][hi] plus...
-                if (hi < F) {
-                    M[ti][hi] += hi > 0 ? M[ti][hi-1] : 0;                      // If 0 < hi < F then the max acorns attaninable by either going down one feet in the same tree, otherwise if hi = 0 then 0 (because there is no possible lower height)
-                } else {
-                    M[ti][hi] += max(M[ti][hi-1], hMax[hi-F]);                  // Otherwise, the max acorns attainable by either going down one feet in the same tree or jumping to another tree and dropping F feet at once
-                }
-                hMax[hi] = max(hMax[hi], M[ti][hi]);                            // Save the max acorns at height hi across all trees
+        for (int h = 0; h<H; h++){
+            for (int t = 0; t<T; t++){
+                M[t][h] += h<F ? (h>0 ? M[t][h-1]:0):max(M[t][h-1], M[T][h-F]); // To get M[t][h] take the amount of acorns from tree t and height h and sum the following to it: if 0 < h < F then the max acorns attaninable by going down one feet in the same tree, if h = 0 then 0 (because there is no possible lower height); otherwise, if h >= F, the max acorns attainable by either going down one foot in the same tree or jumping to another tree and dropping F feet at once
+                M[T][h] = max(M[T][h], M[t][h]);                                // Save the max acorns attainable from height h across all trees
             }
-        }                                                                       // By the end, the memorization matrix is fully computed
-        for (int i = 0; i < T; i++){
-            res = max(res, M[i][H-1]);                                          // Out of all possible ending positions, choose the one with most acorns as the test case result
-        }
-        cout << res << '\n';                                                    // Output test case result to stdout
+        }                                                                       // By the end, the memorization matrix and array are fully computed
+        cout << M[T][H-1] << '\n';                                              // Output test case result to stdout
     }
-    cin >> c;                                                                   // Parse trailing 0
+    cin >> C;                                                                   // Parse trailing 0
     return 0;
 }
